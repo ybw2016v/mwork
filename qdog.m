950e75fc72a872731ebc0709f9d056df8ec67f10
yu <ybw2016v@126.com>
2017-08-04 11:29:43 +0800

    NULL

--- qdog.m
+++ qdog.m
@@ -1,7 +1,17 @@
-x=pi/8:0.01:pi/2;
+x=pi/8:0.008:pi/2;
+ff=0.004;
+%x=pi/4;
 w=15;
 %w=11:0.01:16;
 %k=0.9:0.01:1.1;
 k=4;
 z=(k-1)*cos(x).*cos(x)*9.8./((k-1)*w.*w*sin(x).*sin(x));
-plot(x,z.*tan(x),'-')
+l=z.*tan(x);
+plot(x,l,'-');
+hold on
+for i=1:30
+    xx(i)=x(4*i);
+    yy(i)=l(4*i)+(ff/2-ff*randn);
+    i=i+1;
+end
+plot(xx,yy,'r*');
