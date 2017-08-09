2063f7a9ce2a6963e9b2da4543c9e41cd47878fc
yu <ybw2016v@126.com>
2017-08-09 21:21:59 +0800

    NULL

--- hhdog.m
+++ hhdog.m
@@ -1,5 +1,24 @@
-x=0:0.01:0.5
+x=0:0.01:1.5;
+d=0.03;
+c=0.1;
+b=0.1;
+l=0;
 y = d/c - ((l+d)*exp(-x*(b/2 - (b*b - 4*c)^(1/2)/2)))/(2*c) - ((l+d)*exp(-x*(b/2 + (b*b - 4*c)^(1/2)/2)))/(2*c);
-plot(x*3,y/100+0.05,'-');
+plot(x,y*0.8,'-');
 hold on
-plot(x*3,time/100+0.05,'r*');
+for pp=1:30
+    if pp<10
+        dog=0.005;
+    elseif pp>=10 & pp<20
+        dog=0.002;
+    elseif pp>=20
+        dog=0.005;
+    end
+        
+   t(pp)=x(5*pp);
+   hl(pp)=y(5*pp)+(dog*2*pp*0.01+dog*randn*pp*0.03)*2;
+   pp=pp+1;
+    
+    
+end
+plot(t,hl*0.8,'r*');
--- pptdog.m
+++ pptdog.m
@@ -1,14 +1,14 @@
-l=((x-x1).*(x-x1)+(y-y1).*(y-y1)).^0.5;
+l=((x3-x2).*(x3-x2)+(y3-y2).*(y3-y2)).^0.5;
 
-%figure;plot(t,l,'-',);grid on;
+figure;plot(t2,l,'r^');grid on;
 
-for hh=1:300
+for hh=1:27
    ll(hh)=l(hh);
-   v(hh)=(l(hh+1)-l(hh))./t(hh);
-   tt(hh)=t(hh);
+   v(hh)=(l(hh+1)-l(hh))./(t(hh+1)-t(hh));
+   tt(hh)=t2(hh);
    %plot(t(hh),l,'r-');
    %hold on
     
 end
-figure;plot(tt,v,'-');grid on;
+figure;plot(tt,v,'r*-');grid on;
 figure;plot(tt,ll,'-');
